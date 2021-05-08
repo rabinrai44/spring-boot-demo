aefa64a903ca9a6270a280d367a5ea40f92195c9
Rabin Rai <rabinrai44@gmail.com>
2021-05-08 16:41:10 -0400

    Added InfoApp class to print application information and customize properties

---
+++ src/main/java/com/zalostation/springbootdemo/customer/CustomerConfiguration.java
@@ -0,0 +1,29 @@
+package com.zalostation.springbootdemo.customer;
+
+import com.zalostation.springbootdemo.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class CustomerConfiguration {
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    CommandLineRunner commandLineRunner(InfoApp infoApp) {
+        return args -> {
+            System.out.println("Command line runner hooray!");
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.app.version"));
+            System.out.println(infoApp);
+        };
+    }
+}
